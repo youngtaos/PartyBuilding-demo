37d64c893d3d3fcc41a320561328132947a5a6f7
鸡杂不下面 <1283374626@qq.com>
2023-12-03 19:54:13 +0800

    fix: ajust newData show

--- src/front/pages/Home/Components/Banner/index.tsx
+++ src/front/pages/Home/Components/Banner/index.tsx
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
-import { Carousel } from "antd";
 import styles from "./styles.module.scss";
-import banner1 from "../../../../static/img/banner1.jpg";
 import banner2 from "../../../../static/img/banner2.jpg";
-import banner3 from "../../../../static/img/banner3.jpg";
-import banner4 from "../../../../static/img/banner4.jpg";
 import OverlappingDivsComponent from "../Book/index";
 import { SchemaData } from "../Article";
 
@@ -15,6 +11,7 @@ const Banner: React.FC<{
     <div className={styles.wrapper}>
       <div className={styles.imgBox}>
         <OverlappingDivsComponent data={props.schema} />
+
         <img src={banner2} alt=""></img>
       </div>
     </div>
--- src/front/pages/Home/Components/Book/index.tsx
+++ src/front/pages/Home/Components/Book/index.tsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion/dist/framer-motion";
 import { fadeIn, staggerContainer } from "../../../../util/motion";
 import { Image } from "antd";
 import { SchemaData } from "../Article";
+import { SyncOutlined } from "@ant-design/icons";
 
 interface OverlappingDivsComponentProps {
   data: SchemaData[];
@@ -13,66 +14,75 @@ interface OverlappingDivsComponentProps {
 const OverlappingDivsComponent: React.FC<OverlappingDivsComponentProps> = ({
   data,
 }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
+  // const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentData, setCurrentData] = useState(data);
+  const [rotate, setRotate] = useState(0);
+  function nextPage() {
+    const temp = data;
+    const firstData = data[0];
+    temp.shift();
+    temp.push(firstData);
+    setCurrentData(temp.slice(0, 6).reverse());
+    console.log(temp.slice(0, 6).reverse());
+    setRotate(rotate + 90);
+  }
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, [data.length]);
-
+    setCurrentData(data.slice(0, 6).reverse());
+  }, [data]);
   return (
-    <motion.div
-      className={styles.wrapper}
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-    >
-      {/* <a id="skill" className="anchor2"></a> */}
-      <div
-        className={`paddings yPaddings flexCenter innerWidth ${styles.container}`}
+    <>
+      <motion.div
+        className={styles.wrapper}
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
       >
-        <div>
-          {data
-            .slice(0, 6)
-            .reverse()
-            .map((item, index) => {
-              // if (index >= 8) {
-              //   return null;
-              // }
+        {/* <a id="skill" className="anchor2"></a> */}
+
+        <div
+          className={`paddings yPaddings flexCenter innerWidth ${styles.container}`}
+        >
+          <div>
+            {currentData.map((item, index) => {
+              console.log(index, "index");
               return (
                 <motion.div
-                  variants={fadeIn("right", "tween", (index + 1) * 0.2, 1)}
-                  className={styles.skill}
+                  variants={fadeIn("left", "tween", (index + 1) * 0.2, 1)}
                   key={index}
                 >
                   <div
                     className={styles.card}
                     style={{
-                      top: `${index * 10 + 280}px`,
-                      left: `${index * 10 + 100}px`,
+                      top: "50vh",
+                      left: `${60}vw`,
                     }}
                   >
                     <Image
                       src={item?.imgSrc}
                       alt="抱歉，获取不到对应图片"
-                      width={380}
-                      height={220}
+                      width={"30vw"}
+                      height={"45vh"}
                       preview={false}
                       fallback=""
                     ></Image>
-
+                    {index === currentData.length - 1 && (
+                      <SyncOutlined
+                        className={styles.cardSync}
+                        onClick={nextPage}
+                        rotate={rotate}
+                        style={{ color: "white" }}
+                      />
+                    )}
                     <div className={styles.cardTittle}>{item?.title}</div>
                   </div>
                 </motion.div>
               );
             })}
+          </div>
         </div>
-      </div>
-    </motion.div>
+      </motion.div>
+    </>
   );
 };
 
