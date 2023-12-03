9716734d38f32b2496985e6f235665c5d47ce0d6
鸡杂不下面 <1283374626@qq.com>
2023-12-03 22:05:35 +0800

    fix: update banner and animation

--- src/front/pages/Home/Components/Banner/index.tsx
+++ src/front/pages/Home/Components/Banner/index.tsx
@@ -3,17 +3,15 @@ import styles from "./styles.module.scss";
 import banner2 from "../../../../static/img/banner2.jpg";
 import OverlappingDivsComponent from "../Book/index";
 import { SchemaData } from "../Article";
+import Typewriter from "../Typewriter";
 
 const Banner: React.FC<{
   schema: SchemaData[];
 }> = (props) => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.imgBox}>
-        <OverlappingDivsComponent data={props.schema} />
-
-        <img src={banner2} alt=""></img>
-      </div>
+      <Typewriter text={"cqust"} speed={1000} />
+      <OverlappingDivsComponent data={props.schema} />
     </div>
   );
 };
--- src/front/util/motion.js
+++ src/front/util/motion.js
@@ -96,6 +96,25 @@ export const fadeIn = (direction, type, delay, duration) => ({
     },
 });
 
+export const fadeShow = (direction, type, delay, duration) => ({
+    hidden: {
+        x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+        y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+        opacity: 0,
+    },
+    show: {
+        x: 0,
+        y: 0,
+        opacity: 1,
+        transition: {
+            type,
+            delay,
+            duration,
+            ease: "easeOut",
+        },
+    },
+});
+
 export const zoomIn = (delay, duration) => ({
     hidden: {
         scale: 0,
